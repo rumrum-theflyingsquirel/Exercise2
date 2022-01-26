f9507ed6046d43ac1cb00853b9aab11fa8ba2077
Francis <ftrudeau@gmail.com>
2022-01-25 20:25:23 -0500

    cours du 25jan

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
+
+import fetchdogpictures from './fetchdogpictures.js'
---
+++ fetchdogpictures.js
@@ -0,0 +1,15 @@
+//Tentative de faire comme fetchFollowers.js du cours deu 25 Janvier
+
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+const fetchdogpictures = async () => {
+
+
+const response = await()
+const data = await()
+
+}
+
+export default fetchdogpictures
+
