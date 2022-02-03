78f06000c7935117f8ad476895d6843eabf00594
Francis <ftrudeau@gmail.com>
2022-02-03 13:44:49 -0500

    Fetch not working

---
+++ app.js
@@ -0,0 +1,85 @@
+// API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
+
+//
+
+
+
+const RandomizerBtn = document.querySelector(".btn-primary")
+const Img = document.getElementById("dogsAPI")
+const Name = document.getElementById("dogName")
+const Id = document.getElementById("dogId")
+const Age = document.getElementById("dogAge")
+const Registry = document.getElementById("dogRegistry")
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(DogAge());
+console.log(DogId());
+console.log(DogName());
+console.log(DogRegistry());
+console.log(fetchDogImg());
+
+
+
+
+function fetchDogImg(){
+
+const fetchPromise = fetch(url);
+
+fetchPromise.then(response => {
+  return response.json();
+}).then(lienImg => {
+  const hyperlien = lienImg.map(object => object.message).join("\n");
+
+  console.log(hyperlien)
+})
+}
+
+
+function DogAge () {  
+    return Math.floor((Math.random() * 17) + 1)
+    };
+
+
+function DogId () {
+    return Math.floor((Math.random() * 998) + 1)
+    };
+
+
+function DogName () {
+    const NameDog = ["Chase","Marshall","Rocky","Zuma","Skye","Rubble","Everest","Liberty","Tracker","Tuck","Ella","Rex"];
+    return NameDog[Math.floor((Math.random() * 12))]
+    };
+
+
+function DogRegistry () {   
+    return Math.floor((Math.random() * 9000) + 999)
+    };
+
+
+    
+  RandomizerBtn.addEventListener("click", function() {
+
+Img.src = fetchDogImg();
+Id.innerText = DogId();
+Name.innerText = DogName();
+Age.innerText = DogAge() + " years";
+Registry.innerText = DogRegistry();
+  })
