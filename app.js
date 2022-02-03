da5d33db5281c5c5f0d5402045e67287e57a1566
Francis <ftrudeau@gmail.com>
2022-02-02 22:52:30 -0500

    one days remain

--- app.js
+++ app.js
@@ -1,3 +1,88 @@
 // API à utiliser pour les images de chiens: https://dog.ceo/api/breeds/image/random
 
-import fetchdogpictures from './fetchdogpictures.js'
+//
+
+
+
+const RandomizerBtn = document.querySelector(".btn-primary")
+const Img = document.getElementById("dogsAPI")
+const Name = document.getElementById("dogName")
+const Id = document.getElementById("dogId")
+const Age = document.getElementById("dogAge")
+const Registry = document.getElementById("dogRegistry")
+
+const url = 'https://dog.ceo/api/breeds/image/random'
+
+var Test = []
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(DogAge());
+console.log(DogId());
+console.log(DogName());
+console.log(DogRegistry());
+console.log(fetchDogImg());
+
+
+//Name.innerText = DogName();
+
+
+
+
+const init = async () => {
+    const Chien = await fetchDogImg()
+}
+
+
+
+
+      async function fetchDogImg() {
+        const response = await fetch(url)
+        const data = await response.json()
+        return data;
+      }
+
+
+function DogAge () {  
+    return Math.floor((Math.random() * 17) + 1)
+    };
+
+
+function DogId () {
+    return Math.floor((Math.random() * 998) + 1)
+    };
+
+
+function DogName () {
+    const NameDog = ["Chase","Marshall","Rocky","Zuma","Skye","Rubble","Everest","Liberty","Tracker","Tuck","Ella","Rex"];
+    return NameDog[Math.floor((Math.random() * 12))]
+    };
+
+
+function DogRegistry () {   
+    return Math.floor((Math.random() * 9000) + 999)
+    };
+
+  RandomizerBtn.addEventListener("click", function() {
+
+Img.src = Chien;
+Id.innerText = DogId();
+Name.innerText = DogName();
+Age.innerText = DogAge() + " years";
+Registry.innerText = DogRegistry();
+console.log(Image)
+  })
+  window.addEventListener('load', init)
